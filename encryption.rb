514982e9383876267b09b7484f857c559b7eef13
FAyupov <kapterka@bk.ru>
2020-02-20 17:52:58 +0300

    sha2

--- encryption.rb
+++ encryption.rb
@@ -5,6 +5,7 @@ encrypt_item = STDIN.gets.chomp
 puts "Каким способом зашифровать?"
 puts "1. MD5"
 puts "2. SHA1"
+puts "3. SHA2"
 user_choice = STDIN.gets.to_i
 
 result =
@@ -13,6 +14,8 @@ result =
     Digest::MD5.hexdigest(encrypt_item)
   when 2
     Digest::SHA1.hexdigest(encrypt_item)
+  when 3
+    Digest::SHA2.hexdigest(encrypt_item)
   else
     puts "Вы не выбрали способ шифрования!"
   end
